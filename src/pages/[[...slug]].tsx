49f5e59a5f30143a86722dc9f53d45640cda48e6
gorgeousvlad <gorgeousvlad@gmail.com>
2024-01-03 11:49:01 +0100

    fix: temp

--- src/pages/[[...slug]].tsx
+++ src/pages/[[...slug]].tsx
@@ -4,44 +4,63 @@ import path from 'path';
 import {readFile} from 'fs/promises';
 import {ConfigData} from '../shared/models';
 import {preprocess} from '../server/utils';
-import {PageConstructor} from '@gravity-ui/page-constructor';
+import {
+    NavigationItemType,
+    PageConstructor,
+    PageConstructorProvider,
+} from '@gravity-ui/page-constructor';
+import Link from '../ui/components/Link';
+import {useMemo} from 'react';
 
+const DEFAULT_PAGE = 'index';
 const LOCALES = [{lang: 'en'}, {lang: 'de'}];
-const PAGES = ['index', 'page-3', 'pages/page-1', 'pages/page-2'];
+const PAGES = ['/', 'pages/page-1', 'pages/page-2', 'page-3'];
 const PAGES_DIR = path.resolve('.', 'src/pages-data/en/pages');
 
 const ConstructorPage = ({
     //@ts-ignore
-    data,
+    data: {content, navigation},
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
 }): InferGetStaticPropsType<typeof getStaticProps> => {
+    const location = useMemo(() => ({Link}), []);
     return (
-        <html>
-            <body>
-                <PageConstructor content={data} />
-            </body>
-        </html>
+        <PageConstructorProvider location={location}>
+            <PageConstructor content={content} navigation={navigation} />
+        </PageConstructorProvider>
     );
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-    const slug = context.params?.slug as string;
+    const slug = (context.params?.slug as string) || DEFAULT_PAGE;
     const pagePath = Array.isArray(slug) ? slug.join('/') : slug;
+
     const pagePathFull = path.join(PAGES_DIR, `${pagePath}.yaml`);
 
     const pageData = await readFile(pagePathFull, {encoding: 'utf-8'});
     const content = (pageData && yaml.load(pageData)) as ConfigData;
     const processed = preprocess(content, {locale: 'en', pageName: pagePath});
 
+    const navigation = {
+        header: {
+            leftItems: PAGES.map((pageUrl) => ({
+                type: NavigationItemType.Link,
+                url: pageUrl === '/' ? pageUrl : `/${pageUrl}`,
+                text: pageUrl.split('/').pop() || DEFAULT_PAGE,
+            })),
+        },
+    };
     return {
         props: {
-            data: processed,
+            data: {
+                content: processed,
+                navigation,
+            },
         },
     };
 };
 
 export async function getStaticPaths() {
-    return {
+    const paths = {
         paths: PAGES.map((data) =>
             LOCALES.map((locale) => ({
                 params: {
@@ -52,6 +71,8 @@ export async function getStaticPaths() {
         ).flat(),
         fallback: false,
     };
+
+    return paths;
 }
 
 export default ConstructorPage;
